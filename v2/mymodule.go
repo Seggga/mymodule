4781bd4123f403dad691c43fdb0a8277062e2bc7
super_user <asdf@asdf.ru>
2021-02-27 00:47:04 +0300

    commit v2.0.2

--- v2/mymodule.go
+++ v2/mymodule.go
@@ -4,5 +4,5 @@ import _ "github.com/valyala/fasthttp"
 
 //ShowDep returns info about mymodule version and it's dependencies
 func ShowDep() string {
-	return "mymodule v2.0.1, fasthttp v1.21"
+	return "mymodule v2.0.2, fasthttp v1.21"
 }
