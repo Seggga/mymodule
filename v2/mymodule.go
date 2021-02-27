a50ac01a5808dfe2f81f48a1cb3b90279b05672e
super_user <asdf@asdf.ru>
2021-02-27 20:55:09 +0300

    ShowDepv2 function instead of ShowDep

--- v2/mymodule.go
+++ v2/mymodule.go
@@ -3,6 +3,6 @@ package mymodule
 import _ "github.com/valyala/fasthttp"
 
 //ShowDep returns info about mymodule version and it's dependencies
-func ShowDep() string {
+func ShowDepv2() string {
 	return "mymodule v2.0.2, fasthttp v1.21"
 }
